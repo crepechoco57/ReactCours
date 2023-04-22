2b874d1fec0b945f3a9627659f8b09edb7ed33bf
Herberichs <94360739+crepechoco57@users.noreply.github.com>
2023-04-22 14:44:23 +0200

    newCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+// b9c7de5e
 
+const API_URL = "http://www.omdbapi.com?apikey=b9c7de5e";
+const movie1 = {
+  Title: "Amazing Spidercrotte",
+  Year: "2012",
+  imdbID: "tt2586634",
+  Type: "TVSHOW",
+  Poster: "N/A",
+};
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
+  const [movies, setMovies] = useState([]);
+  //constante est une fonction asynq qui crée
+  // une constante - qui fait un fetch dans l'api pour chercher l'element title qui y loge
+  // une constante - qui attend la reponse de ce fetch en json
+  //le tout inséré dans setMovies qui change le statut de movies
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  //effet au chargement
   useEffect(() => {
-    setCounter(100);
+    searchMovies("Spiderman");
   }, []);
+
   return (
-    <div className="App">
-      <button
-        onClick={() => setCounter((etatDuCompteur) => etatDuCompteur - 1)}
-      >
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button
-        onClick={() => setCounter((etatDuCompteur) => etatDuCompteur + 1)}
-      >
-        +
-      </button>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
